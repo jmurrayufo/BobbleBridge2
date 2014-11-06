ba25a9d07813c3a4c5ca94f367534a52aec6a527
John Murray <jmurrayufo@gmail.com>
2014-11-06 01:43:14 -0700

    Better particle effects

--- Unity/BobbleBridge2/Assets/Scripts/ShipControl.cs
+++ Unity/BobbleBridge2/Assets/Scripts/ShipControl.cs
@@ -4,20 +4,41 @@ using System.Collections;
 public class ShipControl : MonoBehaviour {
    public float shipEngineThrust;
    public float shipTurningThrust;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	   if( Input.GetKey(KeyCode.W) )
+   
+   private ParticleSystem shipsEngines;
+   
+   // Use this for initialization
+   void Start () {
+      shipsEngines = gameObject.transform.Find ("Engine Mount/Particle System").gameObject.GetComponent<ParticleSystem>();
+   }
+   
+   // Update is called once per frame
+   void Update () {
+      if( Input.GetKey(KeyCode.W) )
+      {
          gameObject.rigidbody2D.AddRelativeForce( new Vector2(0f,shipEngineThrust) );
-      if( Input.GetKey(KeyCode.S) )
+         if (shipsEngines.isStopped)
+         {
+            shipsEngines.Play();
+            Debug.Log("Start Paticle Effect");
+         }
+      }      
+      else if( Input.GetKey(KeyCode.S) )
          gameObject.rigidbody2D.AddRelativeForce( new Vector2(0f,-shipEngineThrust) );
+      
+      else
+      {
+         if (!shipsEngines.isStopped)
+         {
+            shipsEngines.Stop();
+            Debug.Log("Stop Paticle Effect");
+         }
+      }
+      
       if (Input.GetKey (KeyCode.A))
          gameObject.rigidbody2D.AddTorque ( shipTurningThrust);
+      
       if (Input.GetKey (KeyCode.D))
          gameObject.rigidbody2D.AddTorque (-shipTurningThrust);
-	}
+   }
 }
