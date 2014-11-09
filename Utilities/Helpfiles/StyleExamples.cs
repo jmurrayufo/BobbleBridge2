2652a472b2b95bf6b90c2bd7cca182df0fa6d1fc
John Murray <jmurrayufo@gmail.com>
2014-11-09 00:15:44 -0700

    Begin Style Summery Sheet

---
+++ Utilities/Helpfiles/StyleExamples.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using System.Collections;
+
+public class Global : MonoBehaviour 
+{
+   // Braces go in the same column that will close them!
+
+   // Lines use 3 SPACES as indent, NOT TABS.
+
+
+   // Use this for initialization.
+   void Start () 
+   {
+      int localVar; //
+   }
+
+
+   // Two spaces before a functions
+   // Update is called once per frame.
+   /*
+    * \brief This is the priefing
+    * \detail This is a long detailed description,
+    * it can even be a multi line.
+    * \bug This would be a bug report
+    */ 
+   void Update () 
+   {
+
+   }
+}
