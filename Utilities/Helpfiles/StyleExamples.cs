6fdf86df37dba9f51feb01cb101451c27a2ea6b9
John Murray <jmurrayufo@gmail.com>
2014-11-09 13:42:25 -0700

    Style guide variable examples.

--- Utilities/Helpfiles/StyleExamples.cs
+++ Utilities/Helpfiles/StyleExamples.cs
@@ -7,8 +7,19 @@ public class Global : MonoBehaviour
 
    // Lines use 3 SPACES as indent, NOT TABS.
 
+   // This is an un-initlized public float. It can be seen in the editor, and will default to 0.0f.  
+   public float foo;
 
-   // Use this for initialization.
+   // This is a public int, but it is hidden in the Unity inspector by a [HideInInspector] tag. Each variable like this 
+   //    would need to be tagged to be hidden. 
+   [HideInInspector]
+   public int bar;
+
+   // This is not hidden like above.
+   public int bar2;
+
+
+   // Public functions should start with an uppercase!
    void Start ()
    {
       int localVar; //
