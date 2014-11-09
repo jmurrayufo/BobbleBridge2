1e443e0b10ba99279ff2bd6056f77ca635743808
John Murray <jmurrayufo@gmail.com>
2014-11-09 16:17:32 -0700

    Rework on main menu to be centered!

--- Unity/BobbleBridge2/Assets/Scripts/MenuButtons.cs
+++ Unity/BobbleBridge2/Assets/Scripts/MenuButtons.cs
@@ -5,36 +5,42 @@ public class MenuButtons : MonoBehaviour {
    
    public Texture btnTexture;
    public Texture alienTexture;
-   
-   void Start() {
-   
-   if (!btnTexture) {
-      Debug.LogError("Please assign a texture on the inspector");
-      return;
-   }
-   if (!alienTexture) {
-      Debug.LogError("Please assign a texture on the inspector");
-      return;
-   }
-      
-}
-   
-   void OnGUI() {
 
-      if (GUI.Button(new Rect(70, 10, 50, 50), alienTexture))
+   // \brief 
+   public float mainGUIAreaWidth;
+   public float mainGUIAreaHeight;
+
+   void Start() {
+      if (!btnTexture) {
+         Debug.LogError("Please assign a texture on the inspector");
+         return;
+      }
+      if (!alienTexture) {
+         Debug.LogError("Please assign a texture on the inspector");
+         return;
+      }
+      
+   }
+
+
+   void OnGUI() {
+      GUILayout.BeginArea(new Rect((Screen.width / 2) - (mainGUIAreaWidth / 2), (Screen.height / 2) - (mainGUIAreaHeight / 2), mainGUIAreaWidth, mainGUIAreaHeight));
+
+      if (GUILayout.Button("Start Game"))
       {
          Debug.Log("Clicked the button with an image");
          // Load the level named "HighScore".
          Application.LoadLevel ("PilotScreen");
       }
          
-      if (GUI.Button(new Rect(10, 10, 50, 50), btnTexture))
+      if (GUILayout.Button(btnTexture, GUILayout.Width(30), GUILayout.Height (30)))
          Debug.Log("Clicked the button with an image");
-      
-      if (GUI.Button (new Rect (10, 70, 50, 30), "Quit")) {
-         Debug.Log ("Clicked the button with text");
-         Application.Quit ();
+
+      if (GUILayout.Button("Quit")) {
+         Debug.Log("Clicked the button with text");
+         Application.Quit();
       }
+      GUILayout.EndArea ();
    }
    
 }
