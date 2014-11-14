ca1b3a46574e2e6f7f14be304ddfd8e79c574a68
John Murray <jmurrayufo@gmail.com>
2014-11-13 21:57:41 -0700

    Creation of basic AI ship controller

---
+++ Unity/BobbleBridge2/Assets/Scripts/AI/AIMovementController.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIMovementController : MonoBehaviour {
+
+
+   public GameObject target;
+   public float followDistance;
+   private Rigidbody2D myRidgid;
+   private float myFaceError;
+   private float distanceToTarget;
+   private PID thrustController;
+	
+
+   // Use this for initialization
+	void Start () {
+      myRidgid = GetComponent<Rigidbody2D>();
+      thrustController = new PID(1,0,20);
+	}
+	
+
+	// Update is called once per frame
+	void Update () {
+   }
+
+   // \TODO AI smarts are in the FixedUpdate function. They need to be pulled out to help support porting of network code.
+   void FixedUpdate() 
+   {
+      Vector3 targetPos;
+      Vector3 vectorToTarget;
+      int sign;
+      // TODO This could get moved to another function to make it cleaner
+      targetPos = target.transform.position;
+      vectorToTarget = targetPos - transform.position;
+      sign = (Vector3.Cross( vectorToTarget, transform.up ).z < 0 ) ? -1 : 1;
+      myFaceError = Vector3.Angle( vectorToTarget, transform.up ) * sign;
+
+      if (target!=null)
+      {
+         distanceToTarget = (transform.position - target.transform.position).magnitude;
+      }
+
+      // \TODO we could slightly adjust heading to help cancel out lateral velocity as well. 
+      // Apply Torque to fix heading
+      myRidgid.AddTorque( -Mathf.Clamp( myFaceError, -0.1f, 0.1f) );
+
+      // Don't thrust when we are not even facing the target
+      if (Mathf.Abs (myFaceError) > 15)
+      {
+         // Not aiming correctly, do nothing!
+      }
+
+      // Handle thrust with a PID controller!
+      else
+      {
+         float thrust = -thrustController.Update(followDistance, distanceToTarget, Time.deltaTime);
+
+         myRidgid.AddRelativeForce( new Vector2(0, Mathf.Clamp(thrust, -10f,10f)));
+
+      }
+   }
+
+
+}
---
+++ Unity/BobbleBridge2/Assets/Scripts/Utility/PID.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PID {
+   public float pVal, iVal, dVal;
+   public float integral;
+   public float lastError;
+   private float currentOutput;
+   
+
+   // Constructor
+   public PID(float pVal, float iVal, float dVal) {
+      this.pVal = pVal;
+      this.iVal = iVal;
+      this.dVal = dVal;
+   }
+   
+
+   // Step the controller and return the new current value
+   public float Update(float setpoint, float actual, float timeStep) {
+      float present = setpoint-actual;
+      integral += present*timeStep;
+      float deriv = (present-lastError)/timeStep;
+      lastError = present;
+      currentOutput = present*pVal + integral*iVal + deriv*dVal;
+      return currentOutput;
+   }
+
+
+   // Get the current value without causing an update
+   public float GetCurrentOutput()
+   {
+      return currentOutput;
+   }
+
+
+   // Reset the PID controller to a base state
+   public void Reset( )
+   {
+      lastError = 0;
+      integral = 0;
+   }
+
+
+}
